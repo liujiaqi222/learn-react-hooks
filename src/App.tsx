64100458d6299a20b0d1e092fd27315002ca1286
刘嘉琪 <z473487465@gmail.com>
2022-10-25 14:30:31 +0000

    build: 使用typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        2323
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
