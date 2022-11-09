1e78412bd7396073d498c4e36ac9370aca133fae
liujiaqi222 <2473487465@qq.com>
2022-11-09 21:19:38 +0800

    feat: useEffect学习

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import { UseReducerBasic, UseReducerExample } from "./components/UseReducer"
+import { UseEffectBasic, UseEffectBasic2 } from "./components/UseEffect"
 function App() {
   return (
     <div className="App">
       {/* usereducer */}
-      <UseReducerBasic />
-      <UseReducerExample />
+      <div style={{ display: "none" }}>
+        <UseReducerBasic />
+      </div>
+      <div style={{ display: "none" }}>
+        <UseReducerExample />
+      </div>
+
+      {/* useEffect */}
+      <div style={{ display: "none" }}>
+        {" "}
+        <UseEffectBasic />
+      </div>
+      <UseEffectBasic2 />
     </div>
   )
 }
